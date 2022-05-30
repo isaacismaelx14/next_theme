f1457351f21cacca570e53429d750ab4be0d0048
Isaac Martinez <isaac@minnekdigital.com>
2022-05-29 22:56:07 -0400

    🎉 code examples. add highlight.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
 import '../styles/entry.scss'
 import type { AppProps } from 'next/app'
+import hljs from 'highlight.js';
+import javascript from 'highlight.js/lib/languages/javascript';
+import typescript from 'highlight.js/lib/languages/typescript';
+import html from 'highlight.js/lib/languages/vbscript-html';
+import css from 'highlight.js/lib/languages/css';
+import { useEffect, useState } from 'react';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('html', html);
+hljs.registerLanguage('css', css);
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [theme, setTheme] = useState('light');
+  useEffect(() =>{ hljs.initHighlighting();
+    if(theme === "dark") {
+      require('highlight.js/styles/atom-one-dark.css');
+    }else {
+      require('highlight.js/styles/atom-one-light.css');
+    }
+  }, [theme])
   return <Component {...pageProps} />
 }
 
